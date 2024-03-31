b430ef5c92660c32ab97b4e197f70a8f320944e7
cyanog3n <falseacckkys@gmail.com>
2024-03-31 18:38:29 +0800

    fixes

--- src/main/java/com/cyanogen/experienceobelisk/event/CustomAnvilRecipes.java
+++ src/main/java/com/cyanogen/experienceobelisk/event/CustomAnvilRecipes.java
@@ -2,6 +2,7 @@ package com.cyanogen.experienceobelisk.event;
 
 import com.cyanogen.experienceobelisk.utils.MiscUtils;
 import net.minecraft.ChatFormatting;
+import net.minecraft.nbt.CompoundTag;
 import net.minecraft.network.chat.Component;
 import net.minecraft.network.chat.MutableComponent;
 import net.minecraft.world.item.DyeItem;
@@ -27,7 +28,7 @@ public class CustomAnvilRecipes {
         validFormattingItems.add(Items.NETHER_STAR);
         validFormattingItems.add(Items.TNT);
 
-        if(event.getName() != null && event.getName().length() >= 1){
+        if(event.getName() != null && event.getName().length() >= 1){ //if renaming field isn't empty
             name = Component.literal(event.getName()).withStyle(left.getHoverName().getStyle());
         }
         else{
@@ -35,6 +36,7 @@ public class CustomAnvilRecipes {
         }
 
         if(right.getItem() instanceof DyeItem || validFormattingItems.contains(right.getItem())){
+
             MutableComponent newName = name;
 
             if(right.getItem() instanceof DyeItem dye){
@@ -45,8 +47,7 @@ public class CustomAnvilRecipes {
                     newName = name.withStyle(format);
                 }
             }
-
-            if(validFormattingItems.contains(right.getItem())){
+            else if(validFormattingItems.contains(right.getItem())){
                 int index = validFormattingItems.indexOf(right.getItem());
                 char code = MiscUtils.itemToFormat(index);
                 ChatFormatting format = ChatFormatting.getByCode(code);
@@ -58,14 +59,9 @@ public class CustomAnvilRecipes {
 
             ItemStack output = left.copy();
             output.setHoverName(newName);
-
             event.setOutput(output);
             event.setCost(1);
             event.setMaterialCost(1);
-        }
-
-        if(event.hasResult()){
-            right.setHoverName(name);
         }
     }
 
