a1feade65e3a54388dcc630cc7360235274ef2bc
cyanog3n <falseacckkys@gmail.com>
2024-04-14 22:04:01 +0800

    added config options for anvil recipes

--- src/main/java/com/cyanogen/experienceobelisk/event/EventHandler.java
+++ src/main/java/com/cyanogen/experienceobelisk/event/EventHandler.java
@@ -1,5 +1,6 @@
 package com.cyanogen.experienceobelisk.event;
 
+import com.cyanogen.experienceobelisk.config.Config;
 import net.minecraftforge.event.AnvilUpdateEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 
@@ -8,10 +9,13 @@ public class EventHandler {
     @SubscribeEvent
     public void onAnvilUpdate(AnvilUpdateEvent event){
 
-        CustomAnvilRecipes.nameFormattingRecipes(event);
-        CustomAnvilRecipes.repairRecipes(event);
-        //todo: add config options
+        if(Config.COMMON.formatting.get()){
+            CustomAnvilRecipes.nameFormattingRecipes(event);
+        }
 
+        if(Config.COMMON.anvilRepair.get()){
+            CustomAnvilRecipes.repairRecipes(event);
+        }
     }
 
 }
