53a55316c3d31a2ae314a2a78542e80e4f8a5e2d
cyanog3n <falseacckkys@gmail.com>
2024-06-11 13:25:22 +0800

    fix tags

--- src/main/java/com/cyanogen/experienceobelisk/block/bibliophage/InfectedBookshelfBlock.java
+++ src/main/java/com/cyanogen/experienceobelisk/block/bibliophage/InfectedBookshelfBlock.java
@@ -8,7 +8,6 @@ import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.ExperienceOrb;
 import net.minecraft.world.entity.player.Player;
-import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
@@ -19,14 +18,11 @@ import net.minecraft.world.level.block.entity.BlockEntityTicker;
 import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.material.FluidState;
-import net.minecraft.world.level.storage.loot.LootParams;
 import net.minecraft.world.phys.shapes.CollisionContext;
 import net.minecraft.world.phys.shapes.EntityCollisionContext;
 import net.minecraft.world.phys.shapes.Shapes;
 import net.minecraft.world.phys.shapes.VoxelShape;
 import org.jetbrains.annotations.Nullable;
-
-import java.util.List;
 
 public class InfectedBookshelfBlock extends Block implements EntityBlock {
 
