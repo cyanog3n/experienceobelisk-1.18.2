6d76f5aa19723b12882175f96da7d815041494e9
cyanog3n <falseacckkys@gmail.com>
2024-07-02 23:20:58 +0800

    collision shape

--- src/main/java/com/cyanogen/experienceobelisk/block/LaserTransfiguratorBlock.java
+++ src/main/java/com/cyanogen/experienceobelisk/block/LaserTransfiguratorBlock.java
@@ -9,9 +9,11 @@ import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.MenuProvider;
+import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.player.Inventory;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.inventory.AbstractContainerMenu;
+import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.EntityBlock;
 import net.minecraft.world.level.block.SoundType;
@@ -19,7 +21,12 @@ import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.entity.BlockEntityTicker;
 import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.phys.AABB;
 import net.minecraft.world.phys.BlockHitResult;
+import net.minecraft.world.phys.shapes.BooleanOp;
+import net.minecraft.world.phys.shapes.CollisionContext;
+import net.minecraft.world.phys.shapes.Shapes;
+import net.minecraft.world.phys.shapes.VoxelShape;
 import net.minecraftforge.items.IItemHandler;
 import net.minecraftforge.network.NetworkHooks;
 import org.jetbrains.annotations.Nullable;
@@ -51,6 +58,22 @@ public class LaserTransfiguratorBlock extends ExperienceReceivingBlock implement
             NetworkHooks.openScreen((ServerPlayer) player, state.getMenuProvider(level,pos), pos);
             return InteractionResult.CONSUME;
         }
+    }
+
+    VoxelShape base = Shapes.create(new AABB(0 / 16D,0 / 16D,0 / 16D,16 / 16D,8 / 16D,16 / 16D));
+    VoxelShape pedestal = Shapes.create(new AABB(2 / 16D,8 / 16D,2 / 16D,14 / 16D,10 / 16D,14 / 16D));
+    VoxelShape emitter = Shapes.create(new AABB(5.5 / 16D,17 / 16D,5.5 / 16D,10.5 / 16D,26 / 16D,10.5 / 16D));
+    VoxelShape baseWhole = Shapes.join(base, pedestal, BooleanOp.OR);
+    VoxelShape whole = Shapes.join(baseWhole, emitter, BooleanOp.OR);
+
+    @Override
+    public boolean collisionExtendsVertically(BlockState state, BlockGetter level, BlockPos pos, Entity collidingEntity) {
+        return true;
+    }
+
+    @Override
+    public VoxelShape getShape(BlockState state, BlockGetter getter, BlockPos pos, CollisionContext context) {
+        return whole;
     }
 
     @Nullable
