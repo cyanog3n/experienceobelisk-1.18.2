b35de7652bbe4aee0d0ebca5e1284e763d75555f
cyanog3n <falseacckkys@gmail.com>
2024-07-04 12:54:17 +0800

    removed anvil recipes (merged with transfigurator)

--- src/main/java/com/cyanogen/experienceobelisk/event/EventHandler.java
+++ src/main/java/com/cyanogen/experienceobelisk/event/EventHandler.java
@@ -1,18 +1,6 @@
 package com.cyanogen.experienceobelisk.event;
 
-import com.cyanogen.experienceobelisk.config.Config;
-import net.minecraftforge.event.AnvilUpdateEvent;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
-
 public class EventHandler {
-
-    @SubscribeEvent
-    public void onAnvilUpdate(AnvilUpdateEvent event){
-
-        if(Config.COMMON.formatting.get()){
-            CustomAnvilRecipes.nameFormattingRecipes(event);
-        }
-    }
 
 
 }
