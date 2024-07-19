b0bd094b117ecfefa9cff40dc9edf6c414b62176
cyanog3n <falseacckkys@gmail.com>
2024-07-19 18:21:23 +0800

    Experience Jelly texture

--- src/main/java/com/cyanogen/experienceobelisk/item/ExperienceJellyItem.java
+++ src/main/java/com/cyanogen/experienceobelisk/item/ExperienceJellyItem.java
@@ -1,5 +1,6 @@
 package com.cyanogen.experienceobelisk.item;
 
+import net.minecraft.network.chat.Component;
 import net.minecraft.sounds.SoundEvents;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResultHolder;
@@ -8,13 +9,16 @@ import net.minecraft.world.food.FoodData;
 import net.minecraft.world.food.FoodProperties;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.item.Rarity;
+import net.minecraft.world.item.TooltipFlag;
 import net.minecraft.world.level.Level;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.List;
 
 public class ExperienceJellyItem extends Item {
 
     public ExperienceJellyItem(Properties properties) {
-        super(properties.food(new FoodProperties.Builder().nutrition(4).saturationMod(0.6F).build()).rarity(Rarity.UNCOMMON));
+        super(properties.food(new FoodProperties.Builder().nutrition(5).saturationMod(0.6F).build()));
     }
 
     @Override
@@ -26,7 +30,7 @@ public class ExperienceJellyItem extends Item {
 
         if(data.needsFood() || player.isCreative()){
 
-            data.setFoodLevel(Math.min(20, foodLevel + 4));
+            data.setFoodLevel(Math.min(20, foodLevel + 5));
             data.setSaturation(Math.min(data.getFoodLevel(), data.getSaturationLevel() + 0.6f));
             player.playSound(SoundEvents.GENERIC_EAT);
 
@@ -34,7 +38,7 @@ public class ExperienceJellyItem extends Item {
                 stack.shrink(1);
             }
 
-            return InteractionResultHolder.sidedSuccess(stack, level.isClientSide);
+            return InteractionResultHolder.consume(stack);
         }
         else{
             return super.use(level, player, hand);
@@ -44,6 +48,13 @@ public class ExperienceJellyItem extends Item {
     @Override
     public boolean isEdible() {
         return false;
+    }
+
+    @Override
+    public void appendHoverText(ItemStack stack, @Nullable Level level, List<Component> tooltip, TooltipFlag flag) {
+
+        tooltip.add(Component.translatable("tooltip.experienceobelisk.experience_jelly.notes"));
+        super.appendHoverText(stack, level, tooltip, flag);
     }
 
 }
