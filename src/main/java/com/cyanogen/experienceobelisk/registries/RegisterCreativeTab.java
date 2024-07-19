6cdd2036f24c0e1f445c7ec3b0f2727dece7ca95
cyanog3n <falseacckkys@gmail.com>
2024-07-19 18:22:34 +0800

    Experience Jelly texture

--- src/main/java/com/cyanogen/experienceobelisk/registries/RegisterCreativeTab.java
+++ src/main/java/com/cyanogen/experienceobelisk/registries/RegisterCreativeTab.java
@@ -61,9 +61,9 @@ public class RegisterCreativeTab {
                         //MISC
                         output.accept(RegisterItems.FORGOTTEN_DUST.get());
                         output.accept(RegisterItems.FORGOTTEN_DUST_BLOCK_ITEM.get());
-                        output.accept(RegisterItems.EXPERIENCE_JELLY.get());
                         output.accept(RegisterItems.NIGHTMARE_BOTTLE.get());
                         output.accept(RegisterItems.DAYDREAM_BOTTLE.get());
+                        output.accept(RegisterItems.EXPERIENCE_JELLY.get());
                         output.accept(RegisterItems.COGNITIUM_BUCKET.get());
                     })
                     .build());
