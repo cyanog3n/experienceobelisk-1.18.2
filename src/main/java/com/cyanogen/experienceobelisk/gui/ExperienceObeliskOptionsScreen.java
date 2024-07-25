9c2603e75ca418b1856723493a403b3b9c34d047
cyanog3n <falseacckkys@gmail.com>
2024-07-25 14:30:05 +0800

    fixed options screen

--- src/main/java/com/cyanogen/experienceobelisk/gui/ExperienceObeliskOptionsScreen.java
+++ src/main/java/com/cyanogen/experienceobelisk/gui/ExperienceObeliskOptionsScreen.java
@@ -64,6 +64,7 @@ public class ExperienceObeliskOptionsScreen extends Screen {
 
         //widgets
         clearWidgets();
+        setupWidgetElements();
         loadWidgetElements();
 
         //descriptors & info
