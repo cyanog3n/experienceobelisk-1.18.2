3ea513593d22878b7b6beecffcc0c45f0689c231
cyanog3n <falseacckkys@gmail.com>
2024-12-09 17:40:39 +0800

    fixes

--- src/main/java/com/cyanogen/experienceobelisk/config/Config.java
+++ src/main/java/com/cyanogen/experienceobelisk/config/Config.java
@@ -74,35 +74,35 @@ public class Config {
 
             builder.push("Infected Bookshelf Settings");
             this.infectedSpawnDelayMin = builder.comment("The minimum spawn delay of Infected Bookshelves in ticks. Default = 150")
-                    .defineInRange("Range", 150, 1, 10000);
+                    .defineInRange("SpawnDelayMin", 150, 1, 10000);
             this.infectedSpawnDelayMax = builder.comment("The maximum spawn delay of Infected Bookshelves in ticks. Default = 250")
-                    .defineInRange("Range", 250, 1, 10000);
+                    .defineInRange("SpawnDelayMax", 250, 1, 10000);
             this.infectedOrbValue = builder.comment("The XP value of spawned orbs. Default = 6")
-                    .defineInRange("Range", 6, 1, 32767);
+                    .defineInRange("OrbValue", 6, 1, 32767);
             this.infectedSpawns = builder.comment("The number of spawns until the bookshelf decays. Default = 100")
-                    .defineInRange("Range", 100, 1, 10000);
+                    .defineInRange("Spawns", 100, 1, 10000);
             builder.pop();
 
             builder.push("Enchanted Bookshelf Settings");
             this.enchantedSpawnDelayMin = builder.comment("The minimum spawn delay of Enchanted Bookshelves in ticks. Default = 100")
-                    .defineInRange("Range", 100, 1, 10000);
+                    .defineInRange("SpawnDelayMin", 100, 1, 10000);
             this.enchantedSpawnDelayMax = builder.comment("The maximum spawn delay of Enchanted Bookshelves in ticks. Default = 300")
-                    .defineInRange("Range", 300, 1, 10000);
+                    .defineInRange("SpawnDelayMax", 300, 1, 10000);
             this.enchantedOrbValue = builder.comment("The XP value of spawned orbs. Default = 12")
-                    .defineInRange("Range", 12, 1, 32767);
+                    .defineInRange("OrbValue", 12, 1, 32767);
             this.enchantedSpawns = builder.comment("The number of spawns until the bookshelf decays. Default = 200")
                     .defineInRange("Range", 200, 1, 10000);
             builder.pop();
 
             builder.push("Archiver's Bookshelf Settings");
             this.archiversSpawnDelayMin = builder.comment("The minimum spawn delay of Archiver's Bookshelves in ticks. Default = 180")
-                    .defineInRange("Range", 180, 1, 10000);
+                    .defineInRange("SpawnDelayMin", 180, 1, 10000);
             this.archiversSpawnDelayMax = builder.comment("The maximum spawn delay of Archiver's Bookshelves in ticks. Default = 220")
-                    .defineInRange("Range", 220, 1, 10000);
+                    .defineInRange("SpawnDelayMax", 220, 1, 10000);
             this.archiversOrbValue = builder.comment("The XP value of spawned orbs. Default = 6")
-                    .defineInRange("Range", 6, 1, 32767);
+                    .defineInRange("OrbValue", 6, 1, 32767);
             this.archiversSpawns = builder.comment("The number of spawns until the bookshelf decays. Default = 200")
-                    .defineInRange("Range", 200, 1, 10000);
+                    .defineInRange("Spawns", 200, 1, 10000);
             builder.pop();
 
         }
